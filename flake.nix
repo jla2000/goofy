dcbb6e79b5ab4ba0c6c44be3779b2aeec9e59046
Jan Lafferton <jan@lafferton.de>
2024-04-09 13:33:26 +0200

    Use rust-overlay

--- flake.nix
+++ flake.nix
@@ -3,20 +3,37 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, rust-overlay }:
     let
       system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ rust-overlay.overlays.default ];
+      };
+      rust = pkgs.rust-bin.stable."latest".default.override {
+        extensions = [ "rust-src" "rust-analyzer" ];
+      };
+      rustPlatform = pkgs.makeRustPlatform {
+        cargo = rust;
+        rustc = rust;
+      };
     in
     {
-      packages.${system}.default = nixpkgs.legacyPackages.${system}.rustPlatform.buildRustPackage {
+      packages.${system}.default = rustPlatform.buildRustPackage {
         pname = "goofy";
         version = "0.1.0";
         src = self;
         cargoLock = {
           lockFile = ./Cargo.lock;
         };
+      };
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs;[
+          rustPlatform
+        ];
       };
     };
 }
