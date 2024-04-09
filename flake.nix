b368ef6df7c13255cf0fabdbeb094d3008ff216f
Jan Lafferton <jan@lafferton.de>
2024-04-09 12:53:41 +0200

    Initialize rust project

--- flake.nix
+++ flake.nix
@@ -1,15 +1,12 @@
 {
-  description = "A very basic flake";
+  description = "Game of life implemented with wgpu";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixpkgs/nixpkgs-unstable";
   };
 
   outputs = { self, nixpkgs }: {
-
     packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
     packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
   };
 }
