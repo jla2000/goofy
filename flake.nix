79d9b2ab62b32a82d20e97f54eb007d2bfa338c0
Jan Lafferton <jan@lafferton.de>
2024-04-17 10:55:26 +0200

    Working devshell

--- flake.nix
+++ flake.nix
@@ -20,20 +20,32 @@
         cargo = rust;
         rustc = rust;
       };
+      libPath = with pkgs; lib.makeLibraryPath [
+        libGL
+        libxkbcommon
+        wayland
+        xorg.libX11
+        xorg.libXcursor
+        xorg.libXi
+        xorg.libXrandr
+      ];
     in
     {
-      packages.${system}.default = rustPlatform.buildRustPackage {
-        pname = "goofy";
+      packages.${system}.default = rustPlatform.buildRustPackage rec {
+        pname = "prismatica";
         version = "0.1.0";
         src = self;
         cargoLock = {
           lockFile = ./Cargo.lock;
         };
+        nativeBuildInputs = [ pkgs.makeWrapper ];
+        postInstall = ''
+          wrapProgram $out/bin/prismatica --prefix LD_LIBRARY_PATH : ${libPath}
+        '';
       };
-      devShells.default = pkgs.mkShell {
-        buildInputs = with pkgs;[
-          rustPlatform
-        ];
+      devShells.${system}.default = pkgs.mkShell {
+        packages = [ rust ];
+        LD_LIBRARY_PATH = libPath;
       };
     };
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::EventLoop,
+    window::WindowBuilder,
+};
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+
+    let event_loop = EventLoop::new().unwrap();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    event_loop.set_control_flow(winit::event_loop::ControlFlow::Wait);
+
+    event_loop
+        .run(move |event, elwt| match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                window_id: _,
+            } => {
+                elwt.exit();
+            }
+            _ => {}
+        })
+        .unwrap();
 }
